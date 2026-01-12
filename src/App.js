a195a756e84218cbd3e43cc0d18f7803720952e5
hrutik-codes <hrutikjagdale@gmail.com>
2026-01-12 18:38:29 +0530

    Initialize React Boxes app using CRA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Box = props => {
+  return <div className={props.className}>{props.Name}</div>
 }
 
-export default App;
+const App = () => {
+  return (
+    <div>
+      <h1 className='heading'>Boxes</h1>
+      <div className='box-container'>
+        <Box className='box box1' Name='Small' />
+        <Box className='box box2' Name='Medium' />
+        <Box className='box box3' Name='Large' />
+      </div>
+    </div>
+  )
+}
+
+export default App
